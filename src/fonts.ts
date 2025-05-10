2531dc15b1cb6f494ef81ba292341af308a80567
braedensellers <braedensellers@gmail.com>
2025-05-10 18:11:43 -0500

    font fix 3

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,32 +1,32 @@
 import { Font } from "@react-pdf/renderer"
 
-// const base_url: string = import.meta.env.BASE_URL
-// const fonts_url: string = `${base_url}/fonts/`
+const base_url: string = import.meta.env.BASE_URL
+const fonts_url: string = `${base_url}fonts/`
 
 Font.register({
     family: "Arial",
     fonts: [
-        { src: `/fonts/arial.ttf`, fontWeight: "normal" },
-        { src: `/fonts/arialbd.ttf`, fontWeight: "bold" },
-        { src: `/fonts/ariali.ttf`, fontStyle: "italic" },
-        { src: `/fonts/arialbi.ttf`, fontWeight: "bold", fontStyle: "italic" },
+        { src: `${fonts_url}arial.ttf`, fontWeight: "normal" },
+        { src: `${fonts_url}arialbd.ttf`, fontWeight: "bold" },
+        { src: `${fonts_url}ariali.ttf`, fontStyle: "italic" },
+        { src: `${fonts_url}arialbi.ttf`, fontWeight: "bold", fontStyle: "italic" },
     ],
 });
 Font.register({
     family: "Calibri",
     fonts: [
-        { src: `/fonts/calibri.ttf`, fontWeight: "normal" },
-        { src: `/fonts/calibrib.ttf`, fontWeight: "bold" },
-        { src: `/fonts/calibrii.ttf`, fontStyle: "italic" },
-        { src: `/fonts/calibribi.ttf`, fontWeight: "bold", fontStyle: "italic" },
+        { src: `${fonts_url}calibri.ttf`, fontWeight: "normal" },
+        { src: `${fonts_url}calibrib.ttf`, fontWeight: "bold" },
+        { src: `${fonts_url}calibrii.ttf`, fontStyle: "italic" },
+        { src: `${fonts_url}calibribi.ttf`, fontWeight: "bold", fontStyle: "italic" },
     ],
 })
 Font.register({
     family: "Times New Roman",
     fonts: [
-        { src: `/fonts/times.ttf`, fontWeight: "normal" },
-        { src: `/fonts/timesbd.ttf`, fontWeight: "bold" },
-        { src: `/fonts/timesi.ttf`, fontStyle: "italic" },
-        { src: `/fonts/timesbi.ttf`, fontWeight: "bold", fontStyle: "italic" },
+        { src: `${fonts_url}times.ttf`, fontWeight: "normal" },
+        { src: `${fonts_url}timesbd.ttf`, fontWeight: "bold" },
+        { src: `${fonts_url}timesi.ttf`, fontStyle: "italic" },
+        { src: `${fonts_url}timesbi.ttf`, fontWeight: "bold", fontStyle: "italic" },
     ],
 })
