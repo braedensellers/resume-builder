280b340ce3eb14da39636c0b655a3d607b1b5624
braedensellers <braedensellers@gmail.com>
2025-05-10 18:06:33 -0500

    font fix test

--- src/fonts.ts
+++ src/fonts.ts
@@ -6,27 +6,27 @@ const fonts_url: string = `${base_url}/fonts/`
 Font.register({
     family: "Arial",
     fonts: [
-        { src: `${fonts_url}arial.ttf`, fontWeight: "normal" },
-        { src: `${fonts_url}arialbd.ttf`, fontWeight: "bold" },
-        { src: `${fonts_url}ariali.ttf`, fontStyle: "italic" },
-        { src: `${fonts_url}arialbi.ttf`, fontWeight: "bold", fontStyle: "italic" },
+        { src: `/fonts/arial.ttf`, fontWeight: "normal" },
+        { src: `/fonts/arialbd.ttf`, fontWeight: "bold" },
+        { src: `/fonts/ariali.ttf`, fontStyle: "italic" },
+        { src: `/fonts/arialbi.ttf`, fontWeight: "bold", fontStyle: "italic" },
     ],
 });
 Font.register({
     family: "Calibri",
     fonts: [
-        { src: `${fonts_url}calibri.ttf`, fontWeight: "normal" },
-        { src: `${fonts_url}calibrib.ttf`, fontWeight: "bold" },
-        { src: `${fonts_url}calibrii.ttf`, fontStyle: "italic" },
-        { src: `${fonts_url}calibribi.ttf`, fontWeight: "bold", fontStyle: "italic" },
+        { src: `/fonts/calibri.ttf`, fontWeight: "normal" },
+        { src: `/fonts/calibrib.ttf`, fontWeight: "bold" },
+        { src: `/fonts/calibrii.ttf`, fontStyle: "italic" },
+        { src: `/fonts/calibribi.ttf`, fontWeight: "bold", fontStyle: "italic" },
     ],
 })
 Font.register({
     family: "Times New Roman",
     fonts: [
-        { src: `${fonts_url}times.ttf`, fontWeight: "normal" },
-        { src: `${fonts_url}timesbd.ttf`, fontWeight: "bold" },
-        { src: `${fonts_url}timesi.ttf`, fontStyle: "italic" },
-        { src: `${fonts_url}timesbi.ttf`, fontWeight: "bold", fontStyle: "italic" },
+        { src: `/fonts/times.ttf`, fontWeight: "normal" },
+        { src: `/fonts/timesbd.ttf`, fontWeight: "bold" },
+        { src: `/fonts/timesi.ttf`, fontStyle: "italic" },
+        { src: `/fonts/timesbi.ttf`, fontWeight: "bold", fontStyle: "italic" },
     ],
 })
